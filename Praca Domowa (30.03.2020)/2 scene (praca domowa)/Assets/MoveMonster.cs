b01be2f83c7edc606911a0ef05611e6612600799
s18712 <61964598+PolinaWawa@users.noreply.github.com>
2020-03-29 16:01:04 +0300

    Praca_Domowa

---
+++ Praca Domowa (30.03.2020)/2 scene (praca domowa)/Assets/MoveMonster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMonster : MonoBehaviour
+{
+    float dirX, moveSpeed = 3f;
+    bool moveRight = true;
+
+
+    void Update()
+    {
+        if (transform.position.x > 1f)
+            moveRight = false;
+        if (transform.position.x < -3f)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+
+}
